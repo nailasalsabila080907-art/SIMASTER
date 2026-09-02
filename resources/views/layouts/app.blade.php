de19f350c405994009b6725e2c0672a6eb16fb7d
r.willy-abyssal <willyanaanjaniraysha@gmail.com>
2026-09-02 11:58:18 +0700

    perbaiki alur

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -380,6 +380,12 @@
 });
 </script>
 @stack('scripts')
-
+<script>
+window.addEventListener('pageshow', function (event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+});
+</script>
 </body>
 </html>
